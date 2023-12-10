8251564875c6752c93426c897da2cb226d7e48f6
Wesley Luntsford <wesluntsford11@gmail.com>
2023-12-10 11:57:02 -0600

    Initial setup. Add User extension.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "ServerResources",
+    platforms: [
+        .iOS(.v16)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ServerResources",
             targets: ["ServerResources"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/appwrite/sdk-for-swift", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/wes111/SharedResourcesClientAndServer", .upToNextMajor(from: "1.0.1"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ServerResources"),
+            name: "ServerResources",
+            dependencies: [
+                .product(name: "Appwrite", package: "sdk-for-swift"),
+                .product(name: "SharedResourcesClientAndServer", package: "SharedResourcesClientAndServer")
+            ]
+        ),
         .testTarget(
             name: "ServerResourcesTests",
             dependencies: ["ServerResources"]),
